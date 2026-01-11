4ad631ca7d28d12b5d7f3c8631f8365846463c9f
Rakesh Yadav <podamekalarakeshyadavb@gmail.com>
2026-01-11 18:38:47 +0530

    updated pathway

--- code/main.py
+++ code/main.py
@@ -5,29 +5,31 @@ import pathway as pw
 from sentence_transformers import SentenceTransformer
 from novel_utils import chunk_text
 
-novels_pw = pw.io.fs.read(
-    path="../books",
-    format="text"
-)
+import sys
 
-train_pw = pw.io.fs.read(
-    path="../data/train.csv",
-    format="csv"
-)
+use_pathway = sys.platform != "win32"
 
-test_pw = pw.io.fs.read(
-    path="../data/test.csv",
-    format="csv"
-)
+if use_pathway:
+    novels_pw = pw.io.fs.read(path="../books", format="text")
+    train_pw = pw.io.fs.read(path="../data/train.csv", format="csv")
+    test_pw = pw.io.fs.read(path="../data/test.csv", format="csv")
 
-novel_rows = novels_pw.to_pylist()
-train_rows = train_pw.to_pylist()
-test_rows = test_pw.to_pylist()
+    novel_rows = novels_pw.to_pylist()
+    train_rows = train_pw.to_pylist()
+    test_rows = test_pw.to_pylist()
 
-full_novel_text = " ".join(row["text"] for row in novel_rows)
+    full_novel_text = " ".join(row["text"] for row in novel_rows)
+    train = pd.DataFrame(train_rows)
+    test = pd.DataFrame(test_rows)
+else:
+    with open("../books/The Count of Monte Cristo.txt", encoding="utf-8", errors="ignore") as f:
+        monte = f.read()
+    with open("../books/In search of the castaways.txt", encoding="utf-8", errors="ignore") as f:
+        castaways = f.read()
 
-train = pd.DataFrame(train_rows)
-test = pd.DataFrame(test_rows)
+    full_novel_text = monte + " " + castaways
+    train = pd.read_csv("../data/train.csv")
+    test = pd.read_csv("../data/test.csv")
 
 train["content"] = train["content"].fillna("")
 test["content"] = test["content"].fillna("")
@@ -45,13 +47,12 @@ dim = novel_embeddings.shape[1]
 novel_index = faiss.IndexFlatL2(dim)
 novel_index.add(np.array(novel_embeddings))
 
-train_embeddings = model.encode(train["content"].tolist(), show_progress_bar=True)
 test_embeddings = model.encode(test["content"].tolist(), show_progress_bar=True)
 
 predictions = []
 
 for emb in test_embeddings:
-    _, _ = novel_index.search(np.array([emb]), k=5)
+    novel_index.search(np.array([emb]), k=5)
     predictions.append(1)
 
 results = pd.DataFrame({
